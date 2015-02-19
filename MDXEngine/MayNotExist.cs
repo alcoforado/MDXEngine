0ff02ce3447da476aef70dda81a276b53204e45d
alcoforado <marcosalcoforado@gmail.com>
2015-02-18 21:25:31 -0500

    Adding May Not Exist

---
+++ MDXEngine/MayNotExist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDXEngine
+{
+    public class MayNotExist<T>  where T: class 
+    {
+        public T Value { get; private set; }
+
+        public bool Exists
+        {
+            get { return Value != null; }
+        }
+
+        public MayNotExist(T t = null)
+        {
+            Value= t;
+        }
+
+      
+
+    }
+}
