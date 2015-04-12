2ded09d420ca6015a1a669c9ccd0d71a100858c7
alcoforado <marcosalcoforado@gmail.com>
2015-04-12 15:47:34 -0400

    Adding the empty project MFreeType. It is a port of the free type library to .NET written in Visual C++

---
+++ MFreeType/MFreeType.h
@@ -0,0 +1,18 @@
+// MFreeType.h
+
+#pragma once
+#include <config\ftstdlib.h>
+
+using namespace System;
+
+namespace MFreeType {
+
+	public ref class MFreeType
+	{
+		MFreeType()
+		{
+
+		}
+
+	};
+}
