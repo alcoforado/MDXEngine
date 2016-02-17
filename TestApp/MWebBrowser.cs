15aca5c6def1106ba2bbad1f5df62f0016e4b3f7
alcoforado <marcosalcoforado@gmail.com>
2016-02-16 21:42:54 -0500

    Addign javascript and missing files

---
+++ TestApp/MWebBrowser.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
+using System.Windows.Threading;
+using Microsoft.Practices.Unity;
+namespace TestApp
+{
+
+    /// </summary>
+    public partial class MWebBrowser : System.Windows.Forms.WebBrowser, IBrowserInterface
+    {
+        MWebBrowserServer _server;
+
+        public MWebBrowser(IUnityContainer container)
+        {
+            RegisterIEBrowserVersion();
+
+
+            //Initialize mwebbrowserserver
+            _server = new MWebBrowserServer(this, container);
+            this.ObjectForScripting = _server;
+
+        }
+       
+
+        void RegisterIEBrowserVersion()
+        {
+           //Get registry;
+           var value = Registry.GetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION","TestApp.exe",null);
+            if (value == null)
+            {
+                Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION","TestApp.exe",11000,RegistryValueKind.DWord);
+                Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION","TestApp.vshost.exe",11000,RegistryValueKind.DWord);
+            }
+           
+        }
+       
+        private String _source;
+        public String SourceURL
+        {
+            get
+            {
+                return _source;
+            }
+
+            set 
+            {
+                _source = value.Trim();
+                if (_source[0] == '/')
+                {
+                    _source=_source.TrimStart('/');
+                    _source = System.IO.Path.GetFullPath(_source);
+                   
+                    var uri = new UriBuilder(_source);
+                    uri.Host = "127.0.0.1";
+                    _source = uri.ToString();
+
+                    var pattern = "/(.):/";
+                    _source = Regex.Replace(_source,pattern,"/$1$$/");
+
+                   
+                }
+                else
+                    _source = new Uri(value).AbsolutePath;
+                this.Navigate(new Uri(_source));
+            }
+
+        }
+           
+       
+       
+      
+
+        public void RunScript(string functionName, params object[] parameters)
+        {
+            this.Document.InvokeScript(functionName, parameters);
+        }
+
+       
+
+    }
+
+
+
+
+     
+
+
+}
+    
---
+++ TestApp/SecWindow.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class SecWindow : Form
+    {
+        MWebBrowser webBrowser;
+        public SecWindow(MWebBrowser browser)
+        {
+            this.webBrowser = browser;
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.Size = new Size(1, 1);
+            this.webBrowser.ClientSize = new Size(1, 1);
+            this.webBrowser.AutoSize = true;
+            this.webBrowser.MinimumSize = new Size(0, 0);
+            this.webBrowser.Width = 10;
+            this.webBrowser.Height = 10;
+            this.webBrowser.ScrollBarsEnabled = false;
+            //this.webBrowser.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            this.StartPosition = FormStartPosition.Manual;
+            
+            this.Controls.Add(webBrowser);
+            InitializeComponent();
+
+            this.Location = Properties.Settings.Default.SecWindowPosition;
+            this.Size = Properties.Settings.Default.SecWindowSize;
+            
+            
+            
+
+            this.HandleDestroyed+= SecWindow_HandleDestroyed;
+          
+        }
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        } 
+
+        public MWebBrowser Browser { get { return webBrowser; } }
+       
+        public void PositionOnRight(Form parentWindow,int width)
+        {
+            
+            StartPosition = FormStartPosition.Manual;
+            Location = new Point(parentWindow.Location.X+parentWindow.ClientSize.Width,parentWindow.Location.Y);
+            Size = new Size(width,parentWindow.Height);
+            Owner=parentWindow;
+            Show();
+        }
+
+        private void SecWindow_HandleDestroyed(object sender, EventArgs e)
+        {
+         
+            Properties.Settings.Default.SecWindowPosition = this.Location;
+            Properties.Settings.Default.SecWindowSize = this.Size;
+            Properties.Settings.Default.Save();
+            this.webBrowser.Dispose();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
