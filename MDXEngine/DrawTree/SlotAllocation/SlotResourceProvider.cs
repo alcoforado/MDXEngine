4e8df391c3aa970eef5da310a051d84e20e41e9e
alcoforado <marcosalcoforado@gmail.com>
2016-03-19 17:55:23 -0400

    adding missing files

---
+++ MDXEngine/DrawTree/SlotAllocation/SlotResourceProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MDXEngine.Interfaces;
+
+namespace MDXEngine.DrawTree.SlotAllocation
+{
+
+
+
+    public partial  class SlotResourceProvider : IDisposable, ISlotResourceProvider
+    {
+        private readonly HLSLProgram _hlsl;
+        private readonly Dictionary<string,SlotPool> _pools;
+        private readonly IShaderResourceFactory _shaderResourceFactory;
+
+
+        public void Dispose()
+            {
+                foreach (var pool in _pools)
+                {
+                    pool.Dispose();
+                }
+            }
+
+        public SlotResourceProvider(IShaderResourceFactory resourceFactory,HLSLProgram hlsl)
+        {
+            _hlsl = hlsl;
+
+            _pools = _hlsl.ProgramResourceSlots.ToList().ToDictionary(x => x.Name, x => new SlotPool(resourceFactory,x));
+        }
+
+
+        public ILoadCommand CreateLoadCommand(SlotRequest request)
+        {
+
+            return new LoadCommand(request, this);
+        }
+
+
+    }
+
+ }
---
+++ MDXEngine/ShaderResources/ShaderResourceFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MDXEngine.DrawTree;
+using MDXEngine.Interfaces;
+using SharpDX.D3DCompiler;
+
+namespace MDXEngine.ShaderResources
+{
+    
+
+    public class ShaderResourceFactory : IShaderResourceFactory
+    {
+        private readonly HLSLProgram _hlsl;
+
+        public ShaderResourceFactory(HLSLProgram hlsl)
+        {
+            _hlsl = hlsl;
+        }
+
+        public IShaderResource CreateForSlot(string slotName)
+        {
+            var slot = _hlsl.ProgramResourceSlots[slotName].Value;
+            var type = slot.ResourceType;
+            switch (type)
+            {
+                case ShaderInputType.ConstantBuffer:
+                    {
+                        var slotType = slot.DataType;
+                        if (slotType == null)
+                            throw new Exception("Slot {0} is a constant buffer but the shader did not specify a type for it");
+
+                        var result = (IShaderResource)typeof(CBufferResource<>).MakeGenericType(slotType)
+                            .GetConstructor(new Type[] { typeof(HLSLProgram) })
+                            .Invoke(new object[] { _hlsl });
+
+                        return result;
+                    }
+                default:
+                    throw new Exception(string.Format("Cannot Construct a ShaderResource of type {0}", type.ToString()));
+            }
+
+        }
+
+
+    }
+}
