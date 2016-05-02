e74236204e93a8c3fd6964dc54fe839b1b978f7d
alcoforado <marcosalcoforado@gmail.com>
2016-05-02 18:25:52 -0400

    forgot to add this file

---
+++ TestApp/Models/ShapesManagerService/Render/SolidColorRenderBase.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Dynamic;
+using MDXEngine;
+using MDXEngine.Painters;
+using MDXEngine.Shapes;
+using SharpDX;
+
+namespace TestApp.Models.ShapesManagerService.Render
+{
+    class SolidColorRenderBase : RenderBaseViewModel
+    {
+        public Color Color { get; set; }
+        
+        public override string GetPainterName()
+        {
+            return "SolidColor";
+        }
+
+        public override object AttachToShader(IDxViewControl _dx, ITopology topology)
+        {
+            var shader = _dx.ResolveShader<ShaderColor3D>();
+            var shape = new Shape3D<VerticeColor>(topology, new CyclicColorizer(Color));
+            shader.Add(shape);
+            return shape;
+        }
+    }
+}
